c01eef43a6de1aa8e2a5948895ecd2716efa4a40
Gavin <gavin@tryingx.com>
2016-03-11 21:43:06 +0800

    Creat Product.

---
+++ DGTools/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DGTools
+//
+//  Created by Gavin on 16/3/11.
+//  Copyright © 2016年 Gavin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
