f52f8e49f22f488c10eecd6880629715f73c75a2
Gavin <gavin@tryingx.com>
2016-03-21 23:05:09 +0800

    将全部延展导入到PCH文件中，添加接口宏定义示例类。

---
+++ DGTools/PrefixHeaders/Macros/NetworkMacro/NetworkMacro.h
@@ -0,0 +1,13 @@
+//
+//  NetworkMacro.h
+//  DGTools
+//
+//  Created by Gavin on 16/3/21.
+//  Copyright © 2016年 com.tryingx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NetworkMacro : NSObject
+
+@end
---
+++ DGTools/PrefixHeaders/Macros/NetworkMacro/NetworkMacro.m
@@ -0,0 +1,17 @@
+//
+//  NetworkMacro.m
+//  DGTools
+//
+//  Created by Gavin on 16/3/21.
+//  Copyright © 2016年 com.tryingx. All rights reserved.
+//
+
+//====================================================================
+//  注意：此类仅作为格式参考，具体的网络请求宏定义可以按照功能模块进行划分。
+//====================================================================
+
+#import "NetworkMacro.h"
+
+@implementation NetworkMacro
+
+@end
